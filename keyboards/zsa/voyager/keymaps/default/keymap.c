0696007d830a341bae94fa83bc9e07a7fb531550
Marcus Buffett <me@mbuffett.com>
2026-09-19 09:03:44 +0200

    Adopt the prose-optimized alpha layout
    Home row becomes N S T R C on the left and A O E I H on the right, with
    the five-alpha swap and semicolon/question-mark swap chosen from the
    personalized cost model.

--- keyboards/zsa/voyager/keymaps/default/keymap.c
+++ keyboards/zsa/voyager/keymaps/default/keymap.c
@@ -7,22 +7,21 @@
 
 enum custom_keycodes {
   RGB_SLD = ZSA_SAFE_RANGE,
-  RESET_STATE,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_voyager(
     KC_BRID, KC_BRIU, KC_VOLD, KC_VOLU, KC_MUTE, KC_MPLY, KC_MPRV, KC_MNXT, KC_PSCR, KC_INS, KC_DEL, KC_PAUS,
-    KC_TAB,         KC_B,           KC_G,           KC_D,           KC_L,           KC_K,                                           KC_Z,           KC_Q,           KC_U,           KC_J,           KC_BSPC,        KC_TRANSPARENT,
-    KC_ESCAPE,      KC_C,           KC_S,           KC_T,           KC_N,           KC_R,                                           KC_A,           KC_O,           KC_E,           KC_I,           KC_F,           KC_ENTER,
-    OSL(3),         OSL(4),         KC_P,           KC_V,           KC_H,           KC_M,                                           KC_X,           KC_CAPS,        RESET_STATE,    KC_Y,           KC_W,           LSFT(KC_ENTER),
+    KC_TAB,         KC_CAPS,        KC_G,           KC_D,           KC_L,           KC_W,                                           KC_Z,           KC_Q,           KC_U,           KC_B,           KC_BSPC,        KC_TRANSPARENT,
+    KC_ESCAPE,      KC_N,           KC_S,           KC_T,           KC_R,           KC_C,                                           KC_A,           KC_O,           KC_E,           KC_I,           KC_H,           KC_ENTER,
+    OSL(3),         OSL(4),         KC_P,           KC_V,           KC_F,           KC_M,                                           KC_X,           KC_K,           KC_J,           KC_Y,           KC_NO,          LSFT(KC_ENTER),
                                                     KC_SPACE,       OSL(2),                                         OSM(MOD_LSFT),  OSL(1)
   ),
   [1] = LAYOUT_voyager(
     KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,
     KC_HASH, KC_AT,          KC_MINUS,       KC_LBRC,        KC_RBRC,        KC_DLR,                                         KC_TILD,        KC_LABK,        KC_RABK,        KC_SLASH,       KC_ASTR,        KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_DQUO,        KC_SCLN,        KC_LPRN,        KC_RPRN,        KC_AMPR,                                        KC_GRAVE,       KC_DOT,         KC_QUOTE,       KC_COMMA,       KC_COLN,        KC_TRANSPARENT,
-    KC_PERCENT, KC_UNDERSCORE,        KC_EQUAL,       KC_LCBR,        KC_RCBR,        KC_PIPE,                                        KC_PLUS,        KC_QUES,        KC_EXLM,        KC_BSLS,        KC_CIRC,        KC_TRANSPARENT,
+    KC_TRANSPARENT, KC_DQUO,        KC_QUES,        KC_LPRN,        KC_RPRN,        KC_AMPR,                                        KC_GRAVE,       KC_DOT,         KC_QUOTE,       KC_COMMA,       KC_COLN,        KC_TRANSPARENT,
+    KC_PERCENT, KC_UNDERSCORE,        KC_EQUAL,       KC_LCBR,        KC_RCBR,        KC_PIPE,                                        KC_PLUS,        KC_SCLN,        KC_EXLM,        KC_BSLS,        KC_CIRC,        KC_TRANSPARENT,
                                                     KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT
   ),
   [2] = LAYOUT_voyager(
@@ -63,7 +62,8 @@ const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM = LAYOUT(
   '*', '*', '*', '*'
 );
 
-const uint16_t PROGMEM mouse_combo[] = {KC_T, KC_N, COMBO_END};
+// Keep the mouse toggle on the same adjacent left home-row keys.
+const uint16_t PROGMEM mouse_combo[] = {KC_T, KC_R, COMBO_END};
 combo_t key_combos[] = {
     COMBO(mouse_combo, TG(5)),
 };
@@ -76,12 +76,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         switch (keycode) {
             case RGB_SLD:
                 rgblight_mode(1);
-                return false;
-            case RESET_STATE:
-                clear_keyboard();
-                clear_oneshot_mods();
-                clear_oneshot_locked_mods();
-                layer_clear();
                 return false;
         }
     }
